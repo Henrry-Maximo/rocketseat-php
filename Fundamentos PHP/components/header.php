abdc0f7a953b4976270d8649275c65b7ee3b69f4
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-11-11 12:08:46 -0300

    ✨Feat: Add header component at app

---
+++ Fundamentos PHP/components/header.php
@@ -0,0 +1,15 @@
+<header
+  class="mx-auto max-w-screen-lg flex items-center justify-between px-3 py-6">
+  <!-- logo -->
+  <div class="font-bold text-xl text-cyan-600">🐧 My Portfolio...</div>
+
+  <!-- links -->
+  <div>
+    <ul class="flex gap-x-3 font-medium text-gray-200">
+      <li><a href="#projects" class="hover:underline">Projects</a></li>
+      <li><a href="#" class="hover:underline">Twitter</a></li>
+      <li><a href="#" class="hover:underline">LinkedIn</a></li>
+      <li><a href="#" class="hover:underline">YouTube</a></li>
+    </ul>
+  </div>
+</header>
