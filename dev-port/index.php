96de83eb9cd01b9415fd57523d551490c8ab678d
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-11-26 16:13:50 -0300

    ✨Feat: Add pages and informations, more background

--- dev-port/index.php
+++ dev-port/index.php
@@ -1,36 +1,92 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+<link rel="stylesheet" href="styles.css">
+</link>
 </head>
 
 <body>
-  <div>
-    <header>
-      <div>
-        <img src="" alt="photo-developer-image" />
-      </div>
+  <div id="background">
+    <section class="page">
+      <header>
+        <div>
+          <img src="" alt="photo-developer-image" />
+        </div>
 
-      <section>
-        <p>Hello World! Meu nome é <span>Henrique Maximo</span> e sou</p>
-        <h1>Desenvolvedor PHP</h1>
-        <p>Transformo necessidades em aplicações reais, evolventes e funcionais. Desenvolvo sistemas através da minha paixão pela tecnologia, contribuindo com soluções inovadoras e eficazes para desafios complexos.</p>
-      </section>
-    </header>
+        <section>
+          <p>Hello World! Meu nome é <span>Henrique Maximo</span> e sou</p>
+          <h1>Desenvolvedor PHP</h1>
+          <p>Transformo necessidades em aplicações reais, evolventes e funcionais. Desenvolvo sistemas através da minha paixão pela tecnologia, contribuindo com soluções inovadoras e eficazes para desafios complexos.</p>
+        </section>
+      </header>
 
-    <footer>
-      <ul>
-        <li>GitHub</li>
-        <li>PHP</li>
-        <li>CSS</li>
-        <li>HTML</li>
-        <li>Javascript</li>
-      </ul>
-    </footer>
+      <footer>
+        <ul>
+          <li>GitHub</li>
+          <li>PHP</li>
+          <li>CSS</li>
+          <li>HTML</li>
+          <li>Javascript</li>
+        </ul>
+      </footer>
+    </section>
   </div>
+
+
+  <section class="page">
+    <div>
+      <span>Meu Trabalho</span>
+      <p>Veja os projetos em destaque</p>
+    </div>
+
+    <div>
+      <ul>
+        <li>
+          <div>
+            <img src="" alt="photo-project-image" />
+          </div>
+
+          <div>
+            <span>Travelgram</span>
+            <p>Rede social onde as pessoas mostram os registros de suas viagens pelo mundo</p>
+            <ul>
+              <li>PHP</li>
+              <li>CSS</li>
+              <li>HTML</li>
+              <li>Javascript</li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+
+  <section class="page">
+    <div>
+      <span>Contato</span>
+      <p>Gostou do meu trabalho?</p>
+      <p>Entre em contato ou acompanhe as minhas redes sociais!</p>
+    </div>
+
+    <div>
+      <ul>
+        <li>
+          <div>
+            <img src="" alt="photo-project-image" />
+          </div>
+
+          <div>
+            <span>Linkedin</span>
+            <img src="" alt="photo-arrow-image" />
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
 </body>
 
 </html>
