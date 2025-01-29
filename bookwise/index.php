18f7cbf7c6db94bb367d2a81af68f42a0cf00a58
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2025-01-29 10:24:16 -0300

    🎉 First Commit: Book Wise

---
+++ bookwise/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Book Wise</title>
+
+  <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+
+  <style type="text/tailwindcss">
+    @theme {
+      --color-clifford: #da373d;
+    }
+    </style>
+</head>
+
+<body>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</body>
+
+</html>
