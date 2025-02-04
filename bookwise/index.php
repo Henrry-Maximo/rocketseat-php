c296724bad7f53c7a5698cc0e135ef1d30416dda
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2025-02-04 10:22:21 -0300

    🎨 Style: Add adjust at layout impving styles of class

--- bookwise/index.php
+++ bookwise/index.php
@@ -31,8 +31,8 @@ $booksDatabaseOnMemory = [
     </style> -->
 </head>
 
-<body class="antialiased h-full">
-  <header class="flex flex-row justify-between p-12 items-center bg-violet-50 border-b-2 border-indigo-500">
+<body class="antialiased h-full bg-gray-100">
+  <header class="flex flex-row justify-between p-12 items-center bg-white border-b-2 border-gray-200">
     <div class="grid grid-cols-2 gap-12 text-center items-center">
       <h1 class="text-3xl font-bold text-gray-700 tracking-wide">Book Wise</h1>
 
@@ -44,67 +44,107 @@ $booksDatabaseOnMemory = [
       </nav>
     </div>
 
-    <ul>
-      <li>
-        <a href="#" class="hover:text-violet-700 underline">
-          Fazer Login
-        </a>
-      </li>
-    </ul>
+    <nav>
+      <ul>
+        <li>
+          <a href="#" class="tracking-wide hover:text-violet-800 hover:bg-gray-100 rounded-full border-2 border-gray-200 px-3 py-2">
+            <i class="ph-fill ph-sign-out"></i>
+          </a>
+        </li>
+      </ul>
+    </nav>
   </header>
 
   <main class="flex flex-col py-12 px-16 gap-12">
     <form class="flex gap-4">
-      <input 
-        type="text" 
-        class="border-stone-200 border-2 bg-stone-100 hover:border-purple-200 text-sm rounded-md flex-grow focus:outline-none px-2 py-2" 
-        placeholder="Digite aqui..." 
-        name="pesquisar"
-      />
-      <button class="bg-purple-200 hover:bg-purple-600 w-12 rounded-md"><i class="ph ph-magnifying-glass"></i></button>
+      <input
+        type="text"
+        class="bg-white border-gray-200 border-2 hover:border-purple-200 text-sm rounded-md focus:outline-none flex-grow  px-2 py-2"
+        placeholder="Digite aqui..."
+        name="pesquisar" />
+      <button type="submit" class="bg-purple-200 hover:bg-purple-600 w-12 rounded-md"><i class="ph ph-magnifying-glass"></i></button>
     </form>
 
-    <section class="grid grid-cols-3 gap-8 mx-28">
-      <div class="flex flex-col border rounded-sm py-4 px-2 mx-auto text-center">
+    <section class="grid grid-cols-3 gap-8">
+      <div class="flex flex-col border border-gray-200 rounded-sm py-4 px-2 mx-auto text-center bg-gray-50">
         <div class="grid grid-cols-2 items-center justify-items-center">
-          <img src="https://www.pimentacultural.com/wp-content/uploads/2024/04/Pimenta_Cultural_explorando-inteligencia.jpg" class="h-16" />
+          <img
+            src="https://www.pimentacultural.com/wp-content/uploads/2024/04/Pimenta_Cultural_explorando-inteligencia.jpg"
+            class="w-1/2 rounded-sm" />
 
           <div>
-            <h2>Explorando a Inteligência Artificial</h2>
-            <span class="ph-thin ph-star"></span>
+            <h2 class="font-semibold">Explorando a Inteligência Artificial</h2>
+
+            <div>
+              <span class="text-xs italic">Autor</span>
+
+              <div>
+                <span class="ph-thin ph-star" />
+                <span class="ph-thin ph-star" />
+                <span class="ph-thin ph-star" />
+                <span class="ph-thin ph-star" />
+                <span class="ph-thin ph-star" />
+              </div>
+            </div>
           </div>
         </div>
 
-        <p class="mt-4">In nova fert animus mutatas dicere formas.</p>
+        <p class="mt-4 text-sm">In nova fert animus mutatas dicere formas.</p>
       </div>
 
-      <div class="flex flex-col border rounded-sm py-4 px-2 mx-auto text-center">
+      <div class="flex flex-col border border-gray-200 rounded-sm py-4 px-2 mx-auto text-center bg-gray-50">
         <div class="grid grid-cols-2 items-center justify-items-center">
-          <img src="https://www.editoradodireito.com.br/media/catalog/product/9/7/9788536533742.80_1.png?optimize=low&bg-color=255,255,255&fit=bounds&height=1000&width=700&canvas=700:1000" class="h-16" />
+          <img
+            src="https://www.pimentacultural.com/wp-content/uploads/2024/04/Pimenta_Cultural_explorando-inteligencia.jpg"
+            class="w-1/2 rounded-sm" />
 
           <div>
-            <h2>O Segredo dos Bancos de Dados</h2>
-            <span class="ph-thin ph-star"></span>
+            <h2 class="font-semibold">Explorando a Inteligência Artificial</h2>
+
+            <div>
+              <span class="text-xs italic">Autor</span>
+
+              <div>
+                <span class="ph-thin ph-star" />
+                <span class="ph-thin ph-star" />
+                <span class="ph-thin ph-star" />
+                <span class="ph-thin ph-star" />
+                <span class="ph-thin ph-star" />
+              </div>
+            </div>
           </div>
         </div>
 
-        <p class="mt-4">Quisque volutpat condimentum velit.</p>
+        <p class="mt-4 text-sm">In nova fert animus mutatas dicere formas.</p>
       </div>
 
-      <div class="flex flex-col border rounded-sm py-4 px-2 mx-auto text-center">
+      <div class="flex flex-col border border-gray-200 rounded-sm py-4 px-2 mx-auto text-center bg-gray-50">
         <div class="grid grid-cols-2 items-center justify-items-center">
-          <img src="https://m.media-amazon.com/images/I/515ANhAjaVL._AC_UF1000,1000_QL80_.jpg" class="h-16" />
+          <img
+            src="https://www.pimentacultural.com/wp-content/uploads/2024/04/Pimenta_Cultural_explorando-inteligencia.jpg"
+            class="w-1/2 rounded-sm" />
 
           <div>
-            <h2>Matemática para Programadores</h2>
-            <span class="ph-thin ph-star"></span>
+            <h2 class="font-semibold">Explorando a Inteligência Artificial</h2>
+
+            <div>
+              <span class="text-xs italic">Autor</span>
+
+              <div>
+                <span class="ph-thin ph-star" />
+                <span class="ph-thin ph-star" />
+                <span class="ph-thin ph-star" />
+                <span class="ph-thin ph-star" />
+                <span class="ph-thin ph-star" />
+              </div>
+            </div>
           </div>
         </div>
 
-        <p class="mt-4">Sed ut perspiciatis unde omnis iste natus error.</p>
+        <p class="mt-4 text-sm">In nova fert animus mutatas dicere formas.</p>
       </div>
-
     </section>
+
   </main>
 </body>
 
